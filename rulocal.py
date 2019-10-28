5552d60ff5ff9ea46979a60bdc0b52e4f15a0c7c
kuzmin71 <e.kuzmin2@g.nsu.ru>
2019-10-28 19:30:05 +0700

    Local

---
+++ rulocal.py
@@ -0,0 +1,8 @@
+Term_of_capital_allocation = 'Срок размещения капитала (лет):'
+Seed_capital = 'Начальный капитал ($):'
+Interest_rate = 'Процентная ставка (%/мес.):'
+Investment_injections = 'Инвестиционные вливания ($/мес.):'
+year = 'год'
+base__amount_of_percent = "|       |   основа   | сумма %  |         |"
+month__investment__per_month__capital = "| месяц | инвестиций | за месяц | капитал |"
+
--- the eighth wonder of the world.py
+++ the eighth wonder of the world.py
@@ -1,15 +1,16 @@
+import rulocal as ru
 year = 1
 i = 1
-years = int(input('Срок размещения капитала (лет):'))
-start_capital = int(input('Начальный капитал ($):'))
-interest = int(input('Процентная ставка (%/мес.):'))
-investments = int(input('Инвестиционные вливания ($/мес.):'))
+years = int(input(ru.Term_of_capital_allocation))
+start_capital = int(input(ru.Seed_capital))
+interest = int(input(ru.Interest_rate))
+investments = int(input(ru.Investment_injections))
 first_income = start_capital*(1+interest/100)
 sum_of_percent = start_capital * interest/100
-print(year, 'год')
+print(year, ru.year)
 print("-------------------------------------------")
-print("|       |   основа   | сумма %  |         |")
-print("| месяц | инвестиций | за месяц | капитал |")
+print(ru.base__amount_of_percent)
+print(ru.month__investment__per_month__capital)
 print("-------------------------------------------")
 print("|  ",i,"  |  "'{:.2f}'.format(first_income - sum_of_percent), "  |  "'{:.2f}'.format(sum_of_percent), "  | "'{:.2f}'.format(first_income), "|")
 capital = first_income
@@ -23,10 +24,10 @@ print("-------------------------------------------")
 print()
 for _ in range(years-1):
     year += 1
-    print(year, 'год')
+    print(year, ru.year)
     print("-------------------------------------------")
-    print("|       |   основа   | сумма %  |         |")
-    print("| месяц | инвестиций | за месяц | капитал |")
+    print(ru.base__amount_of_percent)
+    print(ru.month__investment__per_month__capital)
     print("-------------------------------------------")
     i = 1
     for _ in range(12):
